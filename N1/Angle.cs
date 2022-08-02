a3bfda3d61f208bda9a9dcd24a9e96e2179fe0b6
Алексей Викулин <vaa@megamade.ru>
2022-08-02 11:18:50 +0300

    Add project files.

---
+++ N1/Angle.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N1
+{
+    internal class Angle
+    {
+        int degree;
+        int minute;
+        int second;
+        public bool isCorrect = true;
+        public int Degree
+        {
+            get
+            { 
+                return degree;
+            }
+            set
+            {
+                degree = value % 360; //угол может быть отрицательным
+            }
+        }
+        public int Minute
+        {
+            get
+            {
+                return minute;
+            }
+            set
+            {
+                if (value < 0)
+                    isCorrect = false;
+                else
+                {
+                    Degree += value / 60;
+                    minute = value % 60;
+                }
+                    
+            }
+        }
+        public int Second
+        {
+            get
+            {
+                return second;
+            }
+            set
+            {
+                if (value < 0)
+                    isCorrect = false;
+                else
+                {
+                    Minute += value / 60;
+                    second = value % 60;
+                }
+                    
+            }
+        }
+        public double ToRadians()
+        {
+            return (second + minute * 60 + degree * 3600 * Math.PI) / 180 * 3600;
+        }
+    }
+}
---
+++ N1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N1 //1.   Угол задан с помощью целочисленных значений gradus - градусов, min - угловых минут, sec - угловых секунд. Реализовать класс, в котором указанные значения представлены в виде свойств. Для свойств предусмотреть проверку корректности данных. Класс должен содержать конструктор для установки начальных значений, а также метод ToRadians для перевода угла в радианы. Создать объект на основе разработанного класса. Осуществить использование объекта в программе.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Angle angle;
+            do
+            {
+                Console.WriteLine("Введите значение угла");
+                int d = Convert.ToInt32(Console.ReadLine());
+                int m = Convert.ToInt32(Console.ReadLine());
+                int s = Convert.ToInt32(Console.ReadLine());
+                angle = new Angle(d, m, s);
+            }
+            while (angle.isCorrect == false);
+
+            double radAngle = angle.ToRadians();
+            Console.WriteLine(radAngle);
+            Console.ReadKey();
+        }
+    }
+}
